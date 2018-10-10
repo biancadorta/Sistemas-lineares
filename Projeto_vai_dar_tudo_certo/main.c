d7c05e1d4c7f594e2cfbd8f510779e537ea25809
Bianca Dorta <30930818+biancadorta@users.noreply.github.com>
2018-10-10 18:44:22 -0300

    Colocando na matriz os valores que precisamos
    O código da main já está atribuindo a matriz os coeficientes das equações

--- Projeto_vai_dar_tudo_certo/main.c
+++ Projeto_vai_dar_tudo_certo/main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef
+struct No{
+    char* incognita;
+    size_t tamanho;
+    struct No* prox;
+}No;
+
+void insira(struct No* inicio, char* variavel){
+    if(inicio == NULL){
+        No* Novo = (No*)malloc(sizeof(struct No)); //e para dar erro pois o char* nao foi "instanciado"
+        Novo->incognita = variavel;
+        Novo->prox = NULL;
+    }
+
+}
+
+void resMat(float** mat,int ord){
+
+}
 int main()
 {
     char nome_arquivo[40];
     float** matriz;
-    char** incognitas;
+    char* incognitas;
     int ordem = 0;
     printf("Hello world!\n");
     printf("Digite o nome do arquivo: \n");
@@ -20,7 +39,7 @@ int main()
         printf("Ordem do sistema: %i", ordem);
         //Agora podemos alocar na memoria a matriz, pois temos a ordem
         matriz =(float**)malloc((ordem+1)*sizeof(float*));//+1 porque estamos guardando as variaveis e o valor apos o =s
-        incognitas = (char*)malloc((ordem)*sizeof(char));
+        incognitas = (char*)malloc(ordem*sizeof(char));
 
         int i;
         for(i = 0; i<ordem; i++){
@@ -35,11 +54,12 @@ int main()
             for(i2=0; i2<=ordem-1; i2++){ //le a linha da equacao, neste caso as n primeiras partes
                 //na 1º linha saberemos quais sao as variaveis, logo so precisaremos atribuir ao vetor incognita na primeira vez
                 //que le
-                fscanf(fil, "%f%s ", (*(matriz + j)+ i2), (incognitas+i2)); //FSCANF MECHE COM ENDERECO
+                fscanf(fil, "%f%c ", (*(matriz + j)+ i2), (incognitas+i2)); //FSCANF MECHE COM ENDERECO
                 printf("%.0f",*(*(matriz + j)+ i2));
-                printf("%s\n", *(incognitas+i2));
+                printf("%c\n", *(incognitas+i2));
             }
-            fscanf(fil, "=%f", *(matriz + j)+(i2+1));
+            fscanf(fil, "= %f", *(matriz + j)+(i2+1));
+            printf("%.0f \n",*(*(matriz + j)+(i2+1)));
         }
     }
     return 0;
