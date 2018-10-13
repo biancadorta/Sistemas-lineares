b4660895fd5a4e715f59d6ac67022a4a657727ad
Bianca Dorta <30930818+biancadorta@users.noreply.github.com>
2018-10-12 22:08:33 -0300

    SEM O ACESSO POR INDEXAÇÃO

---
+++ Projeto_vai_dar_tudo_certo/Lista.cpp
@@ -0,0 +1,11 @@
+#include "Lista.h"
+
+Lista::Lista()
+{
+    //ctor
+}
+
+Lista::~Lista()
+{
+    //dtor
+}
---
+++ Projeto_vai_dar_tudo_certo/Lista.h
@@ -0,0 +1,16 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+
+class Lista
+{
+    public:
+        Lista();
+        virtual ~Lista();
+
+    protected:
+
+    private:
+};
+
+#endif // LISTA_H
